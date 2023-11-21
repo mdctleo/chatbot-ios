dfd2eb315fc4589e263b22de8b40e28025c2561a
dominictagg <dominic@tagg.id>
2023-11-21 07:53:33 -0500

    Initial Commit

---
+++ webview/webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewApp.swift
+//  webview
+//
+//  Created by Dominic Damoah on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
